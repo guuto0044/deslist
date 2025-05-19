f4adcc62b03775b28faa45f391f99d994a9688d7
Guto <guuto0044@gmail.com>
2025-05-19 14:38:47 -0300

    Project Started

---
+++ src/test/java/com/guuto0044/deslist/DeslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guuto0044.deslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
